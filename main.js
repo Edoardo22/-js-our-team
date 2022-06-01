e3929e1866c956672e3f33465ece93023b8f5ddf
Edoardo22 <vanaliedoardo@gmail.com>
2022-06-01 17:29:43 +0200

    da finire

--- main.js
+++ main.js
@@ -5,23 +5,23 @@ let team = [
     {
         name: "Gianluca",
         role: "Front-End Developer",
-        photo: "...img/new-team-member-1.jpg"
+        photo: "img/new-team-member-01.jpg"
     },
     {
         name: "Francesco",
         role: "Back-End Developer",
-        photo: "...img/new-team-member-2.jpg"
+        photo: "img/new-team-member-02.jpg"
     },
     {
         name: "Marco",
         role: "Front-End Developer",
-        photo: "...img/new-team-member-3.jpg"
+        photo: "img/new-team-member-03.jpg"
 
     },
     {
         name: "Francesco",
         role: "Back-End Developer",
-        photo: "...img/new-team-member-4.jpg"
+        photo: "img/new-team-member-04.jpg"
     }
 ];
 
@@ -31,11 +31,15 @@ let teamContainer = document.getElementById("team-container");
 let teamCard = document.createElement("div");
 let cardImage = document.createElement("div");
 let cardImg = document.createElement("img");
-cardImg.setAttribute("src", "../img/new-team-member-1.jpg");
+let cardText = document.createElement("div");
+cardImg.setAttribute("src", "img/new-team-member-01.jpg");
 cardImage.classList.add("card-image");
 teamCard.classList.add("team-card");
+cardImage.classList.add("card-text");
 teamContainer.append(teamCard);
 teamCard.append(cardImage);
 cardImage.append(cardImg);
+teamCard.append(cardText);
+
 
 
