94926f322c2d25ce527e1bad07549a3e96d68a17
Edoardo22 <vanaliedoardo@gmail.com>
2022-06-06 15:23:58 +0200

    es finito

--- main.js
+++ main.js
@@ -27,19 +27,26 @@ let team = [
 
 console.table(team);
 
-let teamContainer = document.getElementById("team-container");
-let teamCard = document.createElement("div");
-let cardImage = document.createElement("div");
-let cardImg = document.createElement("img");
-let cardText = document.createElement("div");
-cardImg.setAttribute("src", "img/new-team-member-01.jpg");
-cardImage.classList.add("card-image");
-teamCard.classList.add("team-card");
-cardImage.classList.add("card-text");
-teamContainer.append(teamCard);
-teamCard.append(cardImage);
-cardImage.append(cardImg);
-teamCard.append(cardText);
 
 
+for (let i = 0; i < team.length; i++) {
+    let teamContainer = document.getElementById("team-container");
+    let teamCard = document.createElement("div");
+    let cardImage = document.createElement("div");
+    let cardImg = document.createElement("img");
+    let cardText = document.createElement("div");
+    let h3 = document.createElement("h3");
+    h3.innerHTML = team[i].name;
+    let p = document.createElement("p");
+    p.innerHTML = team[i].role;
 
+    cardImg.setAttribute("src", team[i].photo);
+    cardImage.classList.add("card-image");
+    teamCard.classList.add("team-card");
+    cardText.classList.add("card-text");
+    teamContainer.append(teamCard);
+    teamCard.append(cardImage);
+    cardImage.append(cardImg);
+    teamCard.append(cardText);
+    cardText.append(h3, p);
+}
